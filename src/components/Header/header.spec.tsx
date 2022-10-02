4c2b19314aa373e01062a84f796038d2e42b8312
Junior Dering <juniordering@hotmail.com>
2022-10-02 09:55:40 -0400

    feat: test header component

--- src/components/Header/header.spec.tsx
+++ src/components/Header/header.spec.tsx
@@ -2,33 +2,26 @@ import { render, screen } from "@testing-library/react";
 import { useSession } from "next-auth/react";
 import { Header } from ".";
 
-jest.mock("next-auth/react")
+jest.mock("next-auth/react", () => {
+	return {
+		useSession() {
+			return {
+				data: null,
+			};
+		},
+	};
+});
+// jest.mock("next-auth/react")
 
 describe("Header component", () => {
-	it("renders correctly when signed out", async () => {
-		(useSession as jest.Mock).mockReturnValueOnce({
-			data: null,
-			status: "unauthenticated",
-		});
 
+	it("render header component correctly", () => {
 		render(<Header />);
-		expect(screen.getByText("Entrar com Gitbub")).toBeInTheDocument();
-    screen.debug()
+		expect(screen.getByText("Home")).toBeInTheDocument();
 	});
-
-  // it("render header component", ()=> {
-  //   render(<Header />);
-	// 	expect(screen.getByText("Home")).toBeInTheDocument();
-  // })
+  
+	it("render header component", () => {
+		render(<Header />);
+		expect(screen.getByAltText("Logo Meu board")).toBeInTheDocument();
+	});
 });
-
-// jest.mock("next-auth/react", () => {
-//   return{
-//     useSession(){
-//       return{
-//         data: null,
-//         status: 'unauthenticated'
-//       }
-//     }
-//   }
-// })
