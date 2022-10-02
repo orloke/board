a38cb21a335e355572eebb7b93116aadce542a7c
Junior Dering <juniordering@hotmail.com>
2022-10-02 09:46:06 -0400

    feat: config tests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ const initialOptions = {
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	return (
-		<SessionProvider session={session}>
+		<SessionProvider session={pageProps.session}>
 			<PayPalScriptProvider options={initialOptions} >
 				<Header />
 				<Component {...pageProps} />
