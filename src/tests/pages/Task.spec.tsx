fe9f7dca3dd167bf1956dc15562df2cdcd95fc55
Junior Dering <juniordering@hotmail.com>
2022-10-04 09:29:30 -0400

    feat: test Task page

--- src/tests/pages/Task.spec.tsx
+++ src/tests/pages/Task.spec.tsx
@@ -2,22 +2,25 @@ import { render, screen } from "@testing-library/react";
 import Task, { getServerSideProps } from "../../pages/board/[id]";
 import { getSession } from "next-auth/react";
 
+jest.mock("next-auth/react");
+
 jest.mock("firebase/firestore", () => {
 	return {
-		getDocs: () => {
+		getDoc: () => {
 			return {
-				docs: [
-					{
-						id: 1,
-						data: () => {
-							return {
-								created: {
-									toDate: () => new Date("2022-10-03T18:25:26.024Z"),
-								},
-							};
+				then: () => {
+					return {
+						data:{
+							id: 1,
+							name: 'junior'
 						},
-					},
-				],
+						catch: () => {
+							return{
+								error: 'Deu algum erro'
+							}
+						},
+					}
+				},
 			};
 		},
 		doc: () => null,
@@ -37,11 +40,29 @@ const docSnap = {
 	created: "5 de outubro",
 	tarefa: "Lavar a casa",
 	userId: "1",
-}
+};
 
 describe("Board Page", () => {
 	it("render correctly", () => {
-		render(<Task docSnap={docSnap}/>);
+		render(<Task docSnap={docSnap} />);
 		expect(screen.getByText("Lavar a casa")).toBeInTheDocument();
 	});
-})
+
+	it("loads initial date", async () => {
+		(getSession as jest.Mock).mockReturnValue(null);
+
+		const response = (await getServerSideProps({} as any)) as any;
+		expect(response).toEqual({
+			redirect: { destination: "/board", permanent: false },
+		});
+	});
+
+	it("loads initial date", async () => {
+		(getSession as jest.Mock).mockReturnValue({
+			vip: true,
+		});
+
+		const response = (await getServerSideProps({} as any)) as any;
+		console.log(response);
+	});
+});
